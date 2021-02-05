59cf9ac112e96fd19c60d7ca9be9dfe03c0e2028
pencc <yxpeng@linx-info.com>
2021-02-05 17:12:48 +0800

    add file copy in sdcard

--- app/src/main/java/com/example/fbackup/FullBackup.java
+++ app/src/main/java/com/example/fbackup/FullBackup.java
@@ -7,9 +7,12 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 public class FullBackup {
@@ -23,7 +26,9 @@ public class FullBackup {
                 dir.mkdirs();
             }
 
-            String backupFilePath = sdPath + "/fBackup/" + mPkgName + "/backup.ab";
+            String[] pkgList = {mPkgName};
+
+            String backupFilePath = sdPath + "/fBackup/" + mPkgName + "/backup2.ab";
             File file = new File(backupFilePath);
             if (!file.exists()) {
                 try {
@@ -34,8 +39,6 @@ public class FullBackup {
                 }
             }
             file = new File(backupFilePath);
-            ArrayList pkgList = new ArrayList();
-            pkgList.add(mPkgName);
             //原理是反射调用BackupManagerService.fullbackup
             Class<?> serviceManager = Class.forName("android.os.ServiceManager");
             Method getService = serviceManager.getMethod("getService", String.class);
@@ -52,7 +55,6 @@ public class FullBackup {
             ParcelFileDescriptor parcelFileDescriptor = null;
             try {
                 parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE);
-                String[] strArr = new String[pkgList.size()];
                 Method[] methods = backupManager.getClass().getMethods();
                 int length = methods.length;
                 int i = 0;
@@ -67,26 +69,31 @@ public class FullBackup {
 //                            android.Manifest.permission.BACKUP
 
                         //fullBackup原方法及参数
-                        boolean includeApks = false;
+                        boolean includeApks = true;
                         //obb文件
                         boolean includeObbs = false;
                         //sdcard文件
-                        boolean includeShared = false;
+                        boolean includeShared = true;
+                        boolean doWidgets = false;
                         //是否所有app
                         boolean doAllApps = false;
                         //是否包括系统
                         boolean includeSystem = false;
+                        boolean compress = true;
+                        boolean doKeyValue = false;
                         method.setAccessible(true);
                         method.invoke(backupManager, new Object[]{parcelFileDescriptor,
                                 Boolean.valueOf(includeApks), Boolean.valueOf(includeObbs),
-                                Boolean.valueOf(includeShared), Boolean.valueOf(doAllApps),
-                                Boolean.valueOf(includeSystem), (String[]) pkgList.toArray(strArr)});
+                                Boolean.valueOf(includeShared), Boolean.valueOf(doWidgets),
+                                Boolean.valueOf(doAllApps), Boolean.valueOf(includeSystem),
+                                Boolean.valueOf(compress), Boolean.valueOf(doKeyValue),
+                                pkgList});
                         break;
                     }
                     i++;                }
             } catch (Exception e2) {
                 //addLog("Unable to invoke backup manager for backup" + e2.getMessage());
-                Log.e("backup", e2.getMessage());
+                Log.e("backup", "fail to backup:" + e2.getMessage());
                 if (parcelFileDescriptor != null) {
                     try {
                         parcelFileDescriptor.close();
@@ -104,27 +111,56 @@ public class FullBackup {
         } catch (ClassNotFoundException e) {
             //addLog("ClassNotFoundException : " + e.getMessage());
             e.printStackTrace();
-            Log.e("backup", e.getMessage());
+            Log.e("backup", "fail to backup:" + e.getMessage());
         } catch (NoSuchMethodException e) {
             //addLog("NoSuchMethodException : " + e.getMessage());
             e.printStackTrace();
-            Log.e("backup", e.getMessage());
+            Log.e("backup", "fail to backup:" + e.getMessage());
         } catch (IllegalAccessException e) {
             //addLog("IllegalAccessException : " + e.getMessage());
             e.printStackTrace();
-            Log.e("backup", e.getMessage());
+            Log.e("backup", "fail to backup:" + e.getMessage());
         } catch (InvocationTargetException e) {
             //addLog("InvocationTargetException : " + e.getMessage());
             e.printStackTrace();
-            Log.e("backup", e.getMessage());
+            Log.e("backup", "fail to backup:" + e.getMessage());
+        }
+    }
+
+    private void nioTransferCopy(File source, File target) {
+        FileChannel in = null;
+        FileChannel out = null;
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+        try {
+            inStream = new FileInputStream(source);
+            outStream = new FileOutputStream(target);
+            in = inStream.getChannel();
+            out = outStream.getChannel();
+            in.transferTo(0, in.size(), out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inStream.close();
+                in.close();
+                outStream.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void restoreApk(String mPkgName) {
         try {
             String sdPath = Environment.getExternalStorageDirectory().getPath();
+            String SourceFilePath = sdPath + "/fBackup/" + mPkgName + "/backup2.ab";
             String backupFilePath = sdPath + "/fBackup/" + mPkgName + "/backup.ab";
+            File sfile = new File(SourceFilePath);
             File file = new File(backupFilePath);
+            nioTransferCopy(sfile, file);
+
             //原理是反射调用BackupManagerService.fullbackup
             Class<?> serviceManager = Class.forName("android.os.ServiceManager");
             Method getService = serviceManager.getMethod("getService", String.class);
@@ -140,7 +176,7 @@ public class FullBackup {
             }
             ParcelFileDescriptor parcelFileDescriptor = null;
             try {
-                parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE);
+                parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
                 Method[] methods = backupManager.getClass().getMethods();
                 int length = methods.length;
                 int i = 0;
@@ -154,24 +190,13 @@ public class FullBackup {
 //                           //fullBackup: uid 10065 does not have android.permission.BACKUP.
 //                            android.Manifest.permission.BACKUP
 
-                        //fullBackup原方法及参数
-                        boolean includeApks = false;
-                        //obb文件
-                        boolean includeObbs = false;
-                        //sdcard文件
-                        boolean includeShared = false;
-                        //是否所有app
-                        boolean doAllApps = false;
-                        //是否包括系统
-                        boolean includeSystem = false;
                         method.setAccessible(true);
                         method.invoke(backupManager, parcelFileDescriptor);
                         break;
                     }
                     i++;                }
             } catch (Exception e2) {
-                Log.e("restore", e2.getMessage());
-                //addLog("Unable to invoke backup manager for backup" + e2.getMessage());
+                Log.e("restore", "fail to restore:" + e2.getMessage());
                 if (parcelFileDescriptor != null) {
                     try {
                         parcelFileDescriptor.close();
@@ -186,21 +211,22 @@ public class FullBackup {
                     } catch (IOException e4) {
                     }
                 }
+                file.delete();
             }
         } catch (ClassNotFoundException e) {
-            Log.e("restore", e.getMessage());
+            Log.e("restore", "fail to restore:" + e.getMessage());
             //addLog("ClassNotFoundException : " + e.getMessage());
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
-            Log.e("restore", e.getMessage());
+            Log.e("restore", "fail to restore:" + e.getMessage());
             //addLog("NoSuchMethodException : " + e.getMessage());
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            Log.e("restore", e.getMessage());
+            Log.e("restore", "fail to restore:" + e.getMessage());
             //addLog("IllegalAccessException : " + e.getMessage());
             e.printStackTrace();
         } catch (InvocationTargetException e) {
-            Log.e("restore", e.getMessage());
+            Log.e("restore", "fail to restore:" + e.getMessage());
             //addLog("InvocationTargetException : " + e.getMessage());
             e.printStackTrace();
         }
