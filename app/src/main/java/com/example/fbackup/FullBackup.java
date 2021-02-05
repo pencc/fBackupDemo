f04dc79c1a9da6969cff9572320c69485267c225
pencc <yxpeng@linx-info.com>
2021-02-05 15:12:06 +0800

    add full restore

--- app/src/main/java/com/example/fbackup/FullBackup.java
+++ app/src/main/java/com/example/fbackup/FullBackup.java
@@ -4,6 +4,7 @@ import android.os.Environment;
 import android.os.FileUtils;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,19 +17,20 @@ public class FullBackup {
         try {
             // /storage/emulated/0/backup/{pkgname}/backup.ab
             String sdPath = Environment.getExternalStorageDirectory().getPath();
-            String backupDirPath = sdPath + "/backup/" + mPkgName;
+            String backupDirPath = sdPath + "/fBackup/" + mPkgName;
             File dir = new File(backupDirPath);
             if (!dir.exists()) {
                 dir.mkdirs();
             }
 
-            String backupFilePath = sdPath + "/backup/" + mPkgName + "/backup.ab";
+            String backupFilePath = sdPath + "/fBackup/" + mPkgName + "/backup.ab";
             File file = new File(backupFilePath);
             if (!file.exists()) {
                 try {
                     file.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    Log.e("backup", e.getMessage());
                 }
             }
             file = new File(backupFilePath);
@@ -84,6 +86,7 @@ public class FullBackup {
                     i++;                }
             } catch (Exception e2) {
                 //addLog("Unable to invoke backup manager for backup" + e2.getMessage());
+                Log.e("backup", e2.getMessage());
                 if (parcelFileDescriptor != null) {
                     try {
                         parcelFileDescriptor.close();
@@ -101,13 +104,103 @@ public class FullBackup {
         } catch (ClassNotFoundException e) {
             //addLog("ClassNotFoundException : " + e.getMessage());
             e.printStackTrace();
+            Log.e("backup", e.getMessage());
         } catch (NoSuchMethodException e) {
             //addLog("NoSuchMethodException : " + e.getMessage());
             e.printStackTrace();
+            Log.e("backup", e.getMessage());
         } catch (IllegalAccessException e) {
             //addLog("IllegalAccessException : " + e.getMessage());
             e.printStackTrace();
+            Log.e("backup", e.getMessage());
         } catch (InvocationTargetException e) {
+            //addLog("InvocationTargetException : " + e.getMessage());
+            e.printStackTrace();
+            Log.e("backup", e.getMessage());
+        }
+    }
+
+    public void restoreApk(String mPkgName) {
+        try {
+            String sdPath = Environment.getExternalStorageDirectory().getPath();
+            String backupFilePath = sdPath + "/fBackup/" + mPkgName + "/backup.ab";
+            File file = new File(backupFilePath);
+            //原理是反射调用BackupManagerService.fullbackup
+            Class<?> serviceManager = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManager.getMethod("getService", String.class);
+            getService.setAccessible(true);
+            IBinder iBinder = (IBinder) getService.invoke(null, "backup");
+            Class<?> sub = Class.forName("android.app.backup.IBackupManager$Stub");
+            Method asInterface = sub.getDeclaredMethod("asInterface", IBinder.class);
+            asInterface.setAccessible(true);
+            Object backupManager = asInterface.invoke(null, iBinder);
+            if (backupManager == null) {
+                //addLog("backManager is null");
+                return;
+            }
+            ParcelFileDescriptor parcelFileDescriptor = null;
+            try {
+                parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE);
+                Method[] methods = backupManager.getClass().getMethods();
+                int length = methods.length;
+                int i = 0;
+                while (true) {
+                    if (i >= length) {
+                        break;
+                    }
+                    Method method = methods[i];
+                    if ("adbRestore".equals(method.getName())) {
+                        //需要android.permission.BACKUP权限，hook权限检查
+//                           //fullBackup: uid 10065 does not have android.permission.BACKUP.
+//                            android.Manifest.permission.BACKUP
+
+                        //fullBackup原方法及参数
+                        boolean includeApks = false;
+                        //obb文件
+                        boolean includeObbs = false;
+                        //sdcard文件
+                        boolean includeShared = false;
+                        //是否所有app
+                        boolean doAllApps = false;
+                        //是否包括系统
+                        boolean includeSystem = false;
+                        method.setAccessible(true);
+                        method.invoke(backupManager, parcelFileDescriptor);
+                        break;
+                    }
+                    i++;                }
+            } catch (Exception e2) {
+                Log.e("restore", e2.getMessage());
+                //addLog("Unable to invoke backup manager for backup" + e2.getMessage());
+                if (parcelFileDescriptor != null) {
+                    try {
+                        parcelFileDescriptor.close();
+
+                    } catch (IOException e3) {
+                    }
+                }
+            } finally {
+                if (parcelFileDescriptor != null) {
+                    try {
+                        parcelFileDescriptor.close();
+                    } catch (IOException e4) {
+                    }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            Log.e("restore", e.getMessage());
+            //addLog("ClassNotFoundException : " + e.getMessage());
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            Log.e("restore", e.getMessage());
+            //addLog("NoSuchMethodException : " + e.getMessage());
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            Log.e("restore", e.getMessage());
+            //addLog("IllegalAccessException : " + e.getMessage());
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            Log.e("restore", e.getMessage());
             //addLog("InvocationTargetException : " + e.getMessage());
             e.printStackTrace();
         }
--- app/src/main/java/com/example/fbackup/MainActivity.java
+++ app/src/main/java/com/example/fbackup/MainActivity.java
@@ -1,16 +1,102 @@
 package com.example.fbackup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private final int READ_SDCARD_REQ = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case READ_SDCARD_REQ: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("orpr","onRequestPermissionsResult granted");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Log.i("orpr","onRequestPermissionsResult denied");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void requestPermission(Context context) {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.i("requestPermission","checkSelfPermission");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                Log.i("requestPermission","shouldShowRequestPermissionRationale");
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        READ_SDCARD_REQ);
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        READ_SDCARD_REQ);
+
+            } else {
+                Log.i("requestPermission","requestPermissions");
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        READ_SDCARD_REQ);
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        READ_SDCARD_REQ);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new FullBackup().backupApk("xxx");
+        requestPermission(this);
+
+        Button back_button = (Button)findViewById(R.id.button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FullBackup().backupApk("com.autonavi.minimap");
+            }
+        });
+
+        Button restore_button = (Button)findViewById(R.id.button2);
+        restore_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FullBackup().restoreApk("com.autonavi.minimap");
+            }
+        });
     }
 }
